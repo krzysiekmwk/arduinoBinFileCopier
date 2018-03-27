13ca1db92cbf589186f53bd7d8eb3a5350939664
krzysiekmwk <krzysiek.mwk@gmail.com>
2018-03-27 17:05:14 +0200

    class is working

--- Finder.java
+++ Finder.java
@@ -1,11 +1,6 @@
-package binCopier;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 
 import static java.nio.file.StandardCopyOption.*;
@@ -42,13 +37,6 @@ public class Finder {
 				File fileInUserDir = new File(userFile);
 				System.out.println(fileInUserDir.toString());
 				
-				/*try {
-					copyFile(findBinFile[0], fileInUserDir);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}*/
-				
 				try {
 					Files.copy(findBinFile[0].toPath(), fileInUserDir.toPath(), REPLACE_EXISTING);
 				} catch (IOException e) {
@@ -58,31 +46,4 @@ public class Finder {
 			}
 		}
     }
-	
-	public static void copyFile(File sourceFile, File destFile) throws IOException {
-	    if(!destFile.exists()) {
-	        destFile.createNewFile();
-	    }
-
-	    FileChannel source = null;
-	    FileChannel destination = null;
-	    try {
-	        source = new FileInputStream(sourceFile).getChannel();
-	        destination = new FileOutputStream(destFile).getChannel();
-
-	        // previous code: destination.transferFrom(source, 0, source.size());
-	        // to avoid infinite loops, should be:
-	        long count = 0;
-	        long size = source.size();              
-	        while((count += destination.transferFrom(source, count, size-count))<size);
-	    }
-	    finally {
-	        if(source != null) {
-	            source.close();
-	        }
-	        if(destination != null) {
-	            destination.close();
-	        }
-	    }
-	}
 }
